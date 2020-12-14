4d2a6d37b89100673b15974e4bb61f227f553105
acoss22 <anacseq@gmail.com>
2020-12-14 15:43:09 +0000

    formsubmit

--- images/src/components/SearchBar/SearchBar.js
+++ images/src/components/SearchBar/SearchBar.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term : ''}
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    // }
+
+    // onInputClick(){
+    //     console.log("clicked!");
+    // }
+
+    onFormSubmit(event){
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
     render(){
         return <div className="ui segment">
            
             <form className="ui form">
                 <div className="field">
                     <label>Image Search</label>
-                <input type="text" />
+                <input type="text" 
+                value={this.state.term}
+                onChange={e => this.setState({ term: e.target.value})} 
+                //setState automatically re renders 
+                // this is a controlled element
+                // onChange={(event)=> console.log(event.target.value)}
+                // onChange={this.onInputChange}
+                />
                 </div>
                
             </form>
