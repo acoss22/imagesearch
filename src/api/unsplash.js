02fb776321d22bbe8d41b7d19c677a8c146225ad
acoss22 <anacseq@gmail.com>
2020-12-15 11:42:50 +0000

    my client key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization:
-      'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',
+      'Client-ID 1rCXpwEhbMetXIv53XLQ6Q8BlQXthC5Zod9q9cCJGDY',
   },
 });
