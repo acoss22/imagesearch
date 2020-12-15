0d5bf3874fe0028a1993e1522b36270e73aa5e12
acoss22 <anacseq@gmail.com>
2020-12-15 17:08:15 +0000

    css grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
-  const images = props.images.map((image) =>
-  {
-    return <img src={image.urls.regular} />
-  })
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
+  });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
